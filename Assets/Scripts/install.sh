acdca7a06fd06d8fc480b10248cd1543d72b833a
Oblivion1221 <oblivion1221@hotmail.com>
2017-08-12 23:42:32 +0800

    ....

--- Assets/Scripts/install.sh
+++ Assets/Scripts/install.sh
@@ -1,4 +1,4 @@
-echo 'Downloading from http://download.unity3d.com/download_unity/a2913c821e27/UnityDownloadAssistant-5.6.2f1.dmg: '
-curl -o Unity.dmg http://download.unity3d.com/download_unity/a2913c821e27/UnityDownloadAssistant-5.6.2f1.dmg
-echo 'Installing Unity.dmg’
-sudo installer -dumplog -package Unity.dmg -target /
+echo 'Downloading from http://download.unity3d.com/download_unity/a2913c821e27/MacEditorInstaller/Unity-5.6.2f1.pkg?_ga=2.1274289.1479801571.1502550060-379507740.1499466665: '
+curl -o Unity.pkg http://download.unity3d.com/download_unity/a2913c821e27/MacEditorInstaller/Unity-5.6.2f1.pkg?_ga=2.1274289.1479801571.1502550060-379507740.1499466665
+echo 'Installing Unity.pkg’
+sudo installer -dumplog -package Unity.pkg -target /
